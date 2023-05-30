d4fdc6ea5a29189fcbbc9f037cfd9eb2852b1b19
Kanchan112 <poudel.kanchan38@gmail.com>
2023-05-30 22:19:43 +0545

    Modify datasets for multiple prompt types

--- tools/folder2lmdb.py
+++ tools/folder2lmdb.py
@@ -1,11 +1,13 @@
 import argparse
+import json
 import os
 import os.path as osp
+import warnings
+
 import lmdb
 import pyarrow as pa
-import json
 from tqdm import tqdm
-import warnings
+
 warnings.filterwarnings("ignore")
 
 
@@ -18,7 +20,7 @@ def loads_pyarrow(buf):
 
 
 def raw_reader(path):
-    with open(path, 'rb') as f:
+    with open(path, "rb") as f:
         bin_data = f.read()
     return bin_data
 
@@ -37,19 +39,31 @@ def folder2lmdb(json_data, img_dir, mask_dir, output_dir, split, write_frequency
     isdir = os.path.isdir(lmdb_path)
 
     print("Generate LMDB to %s" % lmdb_path)
-    db = lmdb.open(lmdb_path, subdir=isdir,
-                   map_size=1099511627776 * 2, readonly=False,
-                   meminit=False, map_async=True)
+    db = lmdb.open(
+        lmdb_path,
+        subdir=isdir,
+        map_size=1099511627776 * 2,
+        readonly=False,
+        meminit=False,
+        map_async=True,
+    )
 
     txn = db.begin(write=True)
     tbar = tqdm(json_data)
     for idx, item in enumerate(tbar):
-        img = raw_reader(osp.join(img_dir, item['img_name']))
+        img = raw_reader(osp.join(img_dir, item["img_name"]))
         mask = raw_reader(osp.join(mask_dir, f"{item['segment_id']}.png"))
-        data = {'img': img, 'mask': mask, 'cat': item['cat'],
-                'seg_id': item['segment_id'], 'img_name': item['img_name'],
-                'num_sents': item['sentences_num'], 'sents': [i['sent'] for i in item['sentences']]}
-        txn.put(u'{}'.format(idx).encode('ascii'), dumps_pyarrow(data))
+        data = {
+            "img": img,
+            "mask": mask,
+            "cat": item["cat"],
+            "seg_id": item["segment_id"],
+            "img_name": item["img_name"],
+            "num_sents": item["sentences_num"],
+            "sents": [i["sent"] for i in item["sentences"]],
+            "prompts": item["prompts"],  # edited
+        }
+        txn.put("{}".format(idx).encode("ascii"), dumps_pyarrow(data))
         if idx % write_frequency == 0:
             # print("[%d/%d]" % (idx, len(data_loader)))
             txn.commit()
@@ -57,10 +71,10 @@ def folder2lmdb(json_data, img_dir, mask_dir, output_dir, split, write_frequency
 
     # finish iterating through dataset
     txn.commit()
-    keys = [u'{}'.format(k).encode('ascii') for k in range(idx + 1)]
+    keys = ["{}".format(k).encode("ascii") for k in range(idx + 1)]
     with db.begin(write=True) as txn:
-        txn.put(b'__keys__', dumps_pyarrow(keys))
-        txn.put(b'__len__', dumps_pyarrow(len(keys)))
+        txn.put(b"__keys__", dumps_pyarrow(keys))
+        txn.put(b"__len__", dumps_pyarrow(len(keys)))
 
     print("Flushing database ...")
     db.sync()
@@ -68,32 +82,36 @@ def folder2lmdb(json_data, img_dir, mask_dir, output_dir, split, write_frequency
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='COCO Folder to LMDB.')
-    parser.add_argument('-j', '--json-dir', type=str,
-                        default='',
-                        help='the name of json file.')
-    parser.add_argument('-i', '--img-dir', type=str,
-                        default='refcoco+',
-                        help='the folder of images.')
-    parser.add_argument('-m', '--mask-dir', type=str,
-                        default='refcoco+',
-                        help='the folder of masks.')
-    parser.add_argument('-o', '--output-dir', type=str,
-                        default='refcoco+',
-                        help='the folder of output lmdb file.')
-    parser.add_argument('-s', '--split', type=str,
-                        default='train',
-                        help='the split type.')
+    parser = argparse.ArgumentParser(description="COCO Folder to LMDB.")
+    parser.add_argument(
+        "-j", "--json-dir", type=str, default="", help="the name of json file."
+    )
+    parser.add_argument(
+        "-i", "--img-dir", type=str, default="refcoco+", help="the folder of images."
+    )
+    parser.add_argument(
+        "-m", "--mask-dir", type=str, default="refcoco+", help="the folder of masks."
+    )
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        type=str,
+        default="refcoco+",
+        help="the folder of output lmdb file.",
+    )
+    parser.add_argument(
+        "-s", "--split", type=str, default="train", help="the split type."
+    )
     args = parser.parse_args()
     return args
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     args.split = osp.basename(args.json_dir).split(".")[0]
     os.makedirs(args.output_dir, exist_ok=True)
 
-    with open(args.json_dir, 'rb') as f:
+    with open(args.json_dir, "rb") as f:
         json_data = json.load(f)
 
     folder2lmdb(json_data, args.img_dir, args.mask_dir, args.output_dir, args.split)
--- utils/dataset.py
+++ utils/dataset.py
@@ -11,68 +11,41 @@ from torch.utils.data import Dataset
 from .simple_tokenizer import SimpleTokenizer as _Tokenizer
 
 info = {
-    'refcoco': {
-        'train': 42404,
-        'val': 3811,
-        'val-test': 3811,
-        'testA': 1975,
-        'testB': 1810
+    "refcoco": {
+        "train": 42404,
+        "val": 3811,
+        "val-test": 3811,
+        "testA": 1975,
+        "testB": 1810,
     },
-    'refcoco+': {
-        'train': 42278,
-        'val': 3805,
-        'val-test': 3805,
-        'testA': 1975,
-        'testB': 1798
+    "refcoco+": {
+        "train": 42278,
+        "val": 3805,
+        "val-test": 3805,
+        "testA": 1975,
+        "testB": 1798,
     },
-    'refcocog_u': {
-        'train': 42226,
-        'val': 2573,
-        'val-test': 2573,
-        'test': 5023
-    },
-    'refcocog_g': {
-        'train': 44822,
-        'val': 5000,
-        'val-test': 5000
-    },
-    'kvasir_polyp': {
-        'train': 800,
-        'val': 100,
-        'testA': 100,
-        'testB': 100
-    },
-    'sessile_polyp': {
-        'train': 196,
-        'val': 0,
-        'testA': 0,
-        'testB': 0
-    },
-    'clinicdb_polyp': {
-        'train': 612,
-        'val': 0,
-        'testA': 0,
-        'testB': 0
-    },
-    'bkai_polyp': {
-        'train': 1000,
-        'val': 0,
-        'testA': 0,
-        'testB': 0
-    },
-    'kvasir_polyp_test': {
-        'train': 0,
-        'val': 100,
-        'testA': 0,
-        'testB': 0
-    }
+    "refcocog_u": {"train": 42226, "val": 2573, "val-test": 2573, "test": 5023},
+    "refcocog_g": {"train": 44822, "val": 5000, "val-test": 5000},
+    "kvasir_polyp": {"train": 800, "val": 100, "testA": 100, "testB": 100},
+    "sessile_polyp": {"train": 196, "val": 0, "testA": 0, "testB": 0},
+    "clinicdb_polyp": {"train": 612, "val": 0, "testA": 0, "testB": 0},
+    "bkai_polyp": {"train": 1000, "val": 0, "testA": 0, "testB": 0},
+    "kvasir_polyp_test": {"train": 0, "val": 100, "testA": 0, "testB": 0},
+    "kvasir_polyp_80_10_10": {"train": 800, "val": 100, "testA": 100, "testB": 100},
+    "clinicdb_polyp_80_10_10": {"train": 490, "val": 61, "testA": 61, "testB": 61},
+    "bkai_polyp_80_10_10": {"train": 800, "val": 100, "testA": 100, "testB": 100},
+    "cvc300_polyp_33_0_67": {"train": 0, "val": 20, "testA": 40, "testB": 40},
+    "cvccolondb_polyp_51_0_949": {"train": 0, "val": 20, "testA": 360, "testB": 360},
+    "etis_polyp_10_0_90": {"train": 0, "val": 20, "testA": 176, "testB": 176},
+    "isic_skin_90_10_d": {"train": 800, "val": 100, "testA": 379, "testB": 379},
 }
 _tokenizer = _Tokenizer()
 
 
-def tokenize(texts: Union[str, List[str]],
-             context_length: int = 77,
-             truncate: bool = False) -> torch.LongTensor:
+def tokenize(
+    texts: Union[str, List[str]], context_length: int = 77, truncate: bool = False
+) -> torch.LongTensor:
     """
     Returns the tokenized representation of given input string(s)
 
@@ -96,8 +69,7 @@ def tokenize(texts: Union[str, List[str]],
 
     sot_token = _tokenizer.encoder["<|startoftext|>"]
     eot_token = _tokenizer.encoder["<|endoftext|>"]
-    all_tokens = [[sot_token] + _tokenizer.encode(text) + [eot_token]
-                  for text in texts]
+    all_tokens = [[sot_token] + _tokenizer.encode(text) + [eot_token] for text in texts]
     result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
 
     for i, tokens in enumerate(all_tokens):
@@ -109,7 +81,7 @@ def tokenize(texts: Union[str, List[str]],
                 raise RuntimeError(
                     f"Input {texts[i]} is too long for context length {context_length}"
                 )
-        result[i, :len(tokens)] = torch.tensor(tokens)
+        result[i, : len(tokens)] = torch.tensor(tokens)
 
     return result
 
@@ -123,8 +95,17 @@ def loads_pyarrow(buf):
 
 
 class RefDataset(Dataset):
-    def __init__(self, lmdb_dir, mask_dir, dataset, split, mode, input_size,
-                 word_length):
+    def __init__(
+        self,
+        lmdb_dir,
+        mask_dir,
+        dataset,
+        split,
+        mode,
+        input_size,
+        word_length,
+        prompt_type,
+    ):
         super(RefDataset, self).__init__()
         self.lmdb_dir = lmdb_dir
         self.mask_dir = mask_dir
@@ -133,23 +114,24 @@ class RefDataset(Dataset):
         self.mode = mode
         self.input_size = (input_size, input_size)
         self.word_length = word_length
-        self.mean = torch.tensor([0.48145466, 0.4578275,
-                                  0.40821073]).reshape(3, 1, 1)
-        self.std = torch.tensor([0.26862954, 0.26130258,
-                                 0.27577711]).reshape(3, 1, 1)
+        self.mean = torch.tensor([0.48145466, 0.4578275, 0.40821073]).reshape(3, 1, 1)
+        self.std = torch.tensor([0.26862954, 0.26130258, 0.27577711]).reshape(3, 1, 1)
         self.length = info[dataset][split]
         self.env = None
+        self.prompt_type = prompt_type
 
     def _init_db(self):
-        self.env = lmdb.open(self.lmdb_dir,
-                             subdir=os.path.isdir(self.lmdb_dir),
-                             readonly=True,
-                             lock=False,
-                             readahead=False,
-                             meminit=False)
+        self.env = lmdb.open(
+            self.lmdb_dir,
+            subdir=os.path.isdir(self.lmdb_dir),
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
         with self.env.begin(write=False) as txn:
-            self.length = loads_pyarrow(txn.get(b'__len__'))
-            self.keys = loads_pyarrow(txn.get(b'__keys__'))
+            self.length = loads_pyarrow(txn.get(b"__len__"))
+            self.keys = loads_pyarrow(txn.get(b"__keys__"))
 
     def __len__(self):
         return self.length
@@ -163,16 +145,33 @@ class RefDataset(Dataset):
             byteflow = txn.get(self.keys[index])
         ref = loads_pyarrow(byteflow)
         # img
-        ori_img = cv2.imdecode(np.frombuffer(ref['img'], np.uint8),
-                               cv2.IMREAD_COLOR)
+        ori_img = cv2.imdecode(np.frombuffer(ref["img"], np.uint8), cv2.IMREAD_COLOR)
         img = cv2.cvtColor(ori_img, cv2.COLOR_BGR2RGB)
         img_size = img.shape[:2]
         # mask
-        seg_id = ref['seg_id']
-        mask_dir = os.path.join(self.mask_dir, str(seg_id) + '.png')
+        seg_id = ref["seg_id"]
+        mask_dir = os.path.join(self.mask_dir, str(seg_id) + ".png")
         # sentences
-        idx = np.random.choice(ref['num_sents'])
-        sents = ref['sents']
+        # idx = np.random.choice(ref["num_sents"])
+        # idx = np.random.choice(
+        #     [i for i in range(len(ref["prompts"][f"{self.prompt_type}"]))]
+        # )
+        # sents = ref["sents"]
+        # print(ref["sents"]) # edited
+        if (
+            self.prompt_type == "p7"
+            or self.prompt_type == "p8"
+            or self.prompt_type == "p9"
+        ):
+            sents = ref["prompts"][f"{self.prompt_type}"]
+            # if type(sents) != list:
+            #    sents = list(sents)
+        else:
+            sents = list(str(ref["prompts"][f"{self.prompt_type}"]))  # edited
+            if len(sents) == 0:  # edited
+                sents = [""]  # edited
+            # print(ref["prompts"][f"{self.prompt_type}"] == "")  # edited
+        idx = np.random.choice([i for i in range(len(sents))])
         # transform
         mat, mat_inv = self.getTransformMat(img_size, True)
         img = cv2.warpAffine(
@@ -180,43 +179,44 @@ class RefDataset(Dataset):
             mat,
             self.input_size,
             flags=cv2.INTER_CUBIC,
-            borderValue=[0.48145466 * 255, 0.4578275 * 255, 0.40821073 * 255])
-        if self.mode == 'train':
+            borderValue=[0.48145466 * 255, 0.4578275 * 255, 0.40821073 * 255],
+        )
+        # print(sents)
+        if self.mode == "train":
             # mask transform
-            mask = cv2.imdecode(np.frombuffer(ref['mask'], np.uint8),
-                                cv2.IMREAD_GRAYSCALE)
-            mask = cv2.warpAffine(mask,
-                                  mat,
-                                  self.input_size,
-                                  flags=cv2.INTER_LINEAR,
-                                  borderValue=0.)
-            mask = mask / 255.
+            mask = cv2.imdecode(
+                np.frombuffer(ref["mask"], np.uint8), cv2.IMREAD_GRAYSCALE
+            )
+            mask = cv2.warpAffine(
+                mask, mat, self.input_size, flags=cv2.INTER_LINEAR, borderValue=0.0
+            )
+            mask = mask / 255.0
             # sentence -> vector
             sent = sents[idx]
             word_vec = tokenize(sent, self.word_length, True).squeeze(0)
             img, mask = self.convert(img, mask)
             return img, word_vec, mask
-        elif self.mode == 'val':
+        elif self.mode == "val":
             # sentence -> vector
             sent = sents[0]
             word_vec = tokenize(sent, self.word_length, True).squeeze(0)
             img = self.convert(img)[0]
             params = {
-                'mask_dir': mask_dir,
-                'inverse': mat_inv,
-                'ori_size': np.array(img_size)
+                "mask_dir": mask_dir,
+                "inverse": mat_inv,
+                "ori_size": np.array(img_size),
             }
             return img, word_vec, params
         else:
             # sentence -> vector
             img = self.convert(img)[0]
             params = {
-                'ori_img': ori_img,
-                'seg_id': seg_id,
-                'mask_dir': mask_dir,
-                'inverse': mat_inv,
-                'ori_size': np.array(img_size),
-                'sents': sents
+                "ori_img": ori_img,
+                "seg_id": seg_id,
+                "mask_dir": mask_dir,
+                "inverse": mat_inv,
+                "ori_size": np.array(img_size),
+                "sents": sents,
             }
             return img, params
 
@@ -225,11 +225,13 @@ class RefDataset(Dataset):
         inp_h, inp_w = self.input_size
         scale = min(inp_h / ori_h, inp_w / ori_w)
         new_h, new_w = ori_h * scale, ori_w * scale
-        bias_x, bias_y = (inp_w - new_w) / 2., (inp_h - new_h) / 2.
+        bias_x, bias_y = (inp_w - new_w) / 2.0, (inp_h - new_h) / 2.0
 
         src = np.array([[0, 0], [ori_w, 0], [0, ori_h]], np.float32)
-        dst = np.array([[bias_x, bias_y], [new_w + bias_x, bias_y],
-                        [bias_x, new_h + bias_y]], np.float32)
+        dst = np.array(
+            [[bias_x, bias_y], [new_w + bias_x, bias_y], [bias_x, new_h + bias_y]],
+            np.float32,
+        )
 
         mat = cv2.getAffineTransform(src, dst)
         if inverse:
@@ -242,7 +244,7 @@ class RefDataset(Dataset):
         img = torch.from_numpy(img.transpose((2, 0, 1)))
         if not isinstance(img, torch.FloatTensor):
             img = img.float()
-        img.div_(255.).sub_(self.mean).div_(self.std)
+        img.div_(255.0).sub_(self.mean).div_(self.std)
         # Mask ToTensor
         if mask is not None:
             mask = torch.from_numpy(mask)
@@ -251,13 +253,16 @@ class RefDataset(Dataset):
         return img, mask
 
     def __repr__(self):
-        return self.__class__.__name__ + "(" + \
-            f"db_path={self.lmdb_dir}, " + \
-            f"dataset={self.dataset}, " + \
-            f"split={self.split}, " + \
-            f"mode={self.mode}, " + \
-            f"input_size={self.input_size}, " + \
-            f"word_length={self.word_length}"
+        return (
+            self.__class__.__name__
+            + "("
+            + f"db_path={self.lmdb_dir}, "
+            + f"dataset={self.dataset}, "
+            + f"split={self.split}, "
+            + f"mode={self.mode}, "
+            + f"input_size={self.input_size}, "
+            + f"word_length={self.word_length}"
+        )
 
     # def get_length(self):
     #     return self.length
