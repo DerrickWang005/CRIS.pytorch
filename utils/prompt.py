860f03c3fa61b66d7a663ee3fd10178946f05766
Kanchan112 <poudel.kanchan38@gmail.com>
2023-05-30 22:24:04 +0545

    Prompt Generation

--- utils/prompt.py
+++ utils/prompt.py
@@ -1,19 +1,16 @@
 import os
 import random
-import sys
 
 import cv2
 import numpy as np
 from features_from_img import get_mask_decription
 from PIL import Image
-
-from OFA.single_inference import get_answer, return_model
+from single_inference import get_answer
 
 random.seed(444)
 
 
 def gen_prompt(
-    models,
     image,
     mask,
     class_names,
@@ -41,16 +38,14 @@ def gen_prompt(
     ]
 
     for (attr, q) in vqa_questions:
-        # print(q)
-        ans = get_answer(models, image, mask, q, verbose=True)
+        ans = get_answer(image, mask, q)
         attr_dict["vqa"][attr] = ans
-        # print(ans)
 
     (
         attr_dict["from_mask"]["size"],
         attr_dict["from_mask"]["rel_location"],
         attr_dict["from_mask"]["number"],
-    ) = get_mask_decription(np.array(mask)[:, :, 0])
+    ) = get_mask_decription(np.array(mask))
 
     for attr in attr_dict["from_mask"].keys():
         if attr not in attrs:
@@ -65,21 +60,21 @@ def gen_prompt(
             prompt = f'{attr_dict["from_mask"]["size"]} {attr_dict["vqa"]["color"]} {attr_dict["vqa"]["shape"]} {class_name}, which is {general_description} located in the {attr_dict["from_mask"]["rel_location"]} of this image'
         else:
             prompt = f'{attr_dict["from_mask"]["size"]} {attr_dict["vqa"]["color"]} {attr_dict["vqa"]["shape"]} {class_name}, {general_description}'
-    # print(attr_dict)
+
     return prompt.strip()
 
 
 if __name__ == "__main__":
     class_names = ["polyp"]  # override in datset if needed
     query_names = ["bump"]
-    attrs = ["shape", "abs_location", "rel_location", "color", "number", "size"]
+    attrs = ["shape", "rel_location", "color", "number", "size"]
     g_ds = ["often a bumpy flesh found in rectum"]
 
     ROOT_DIR = "/mnt/Enterprise/PUBLIC_DATASETS/polyp_datasets/Kvasir-SEG"
-    image_number = 3
+    image_number = 1
     image_path = os.path.join(ROOT_DIR, "images_cf", f"{image_number}.jpg")
     mask_path = os.path.join(ROOT_DIR, "masks_cf", f"{image_number}.png")
     prompt = ""
-    models = return_model
+
     with Image.open(image_path) as image, Image.open(mask_path) as mask:
-        prompt = gen_prompt(models, image, mask, class_names, query_names, attrs, g_ds)
+        prompt = gen_prompt(image, mask, class_names, query_names, attrs, g_ds)
